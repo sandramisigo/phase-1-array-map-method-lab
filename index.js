8c4e4676a7bbc74f78f16804559f5e1fb3cd7f7e
Sandra Misigo <misigosandra@gmail.com>
2024-12-07 10:42:43 +0300

    phase-1-array-map-method-lab

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial
+      .split(' ') // .split helps to split into words
+      .map(word => {
+        // .map helps us to Keep the rest of the word intact after the first letter meaning no lowercasing the others so that abbreviations won't be affected.)
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(' '); // .Join is used to join the words back into a single string
+  });
 }
+
+console.log(titleCased());
